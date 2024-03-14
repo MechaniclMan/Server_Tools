58197b505dd6ae65a918d36214bc094569553c1d
RxD <solidfire444@hotmail.com>
2024-03-13 18:46:33 -0700

    clean_irc_log

--- CleanIRCLog/cleanirclog.pl
+++ CleanIRCLog/cleanirclog.pl
@@ -13,8 +13,11 @@ my %to_print = ( msg => 1, action => 1 );
 print PLAYERDATA "Test\n";
 
 for ($result->events) {
-my $line = "$_->{text}";
-chomp $line;
+	my $line = "$_->{text}";
+	chomp $line;
+	$line =~ s/\cC\d{1,2}(?:,\d{1,2})?|[\cC\cB\cI\cU\cR\cO\c_]//g;
+	print PLAYERDATA "$line\n";
+	
 #$line =~ s/\^.{1,7}?m//g;
 #'s/\x1b\[[0-9;]*m//g'
 #$line = s/\x1b\[[0-9;]*m//g;
@@ -29,14 +32,11 @@ chomp $line;
 #[May 01] {022900} 13[0900RCBOT13] 03[TRIGGERBOT]08 
 #if ( $line =~ m/^\[.+\]\s\{.+\}\s\d\[.+\]\s\d(\[.+\])\d\s(.+)/ ) {
 #if ( $line =~ m/\d{2}(\[.+\])\d(.+)/ ) {
-$line =~ s/\cC\d{1,2}(?:,\d{1,2})?|[\cC\cB\cI\cU\cR\cO\c_]//g;
 #if ( $line =~ m/(\[.+\] \{.+\}).+(\[.+\])(.+)/ ) {
 #$line = "$1 $2$3";
 #print PLAYERDATA "$line\n";
 #}
-print PLAYERDATA "$line\n";
-
-
+	
 }
 
 
